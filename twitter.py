f3f1c32464a32875faba8e104b6f47acd73aef6f
Mohamed Anwar <mohamedanwarvic@gmail.com>
2019-06-25 07:32:47 +0200

    adding method documentation in twitter.py

--- twitter.py
+++ twitter.py
@@ -21,4 +21,36 @@ class TwitterCaller():
 									wait_on_rate_limit_notify=True,
 									compression=True)
 
-	
+	def __get_tweet_by_id(self, tweet_id):
+		"""
+		This function is responsible for calling twitter API and retireve 
+		tweets from based on the tweet_id
+		Args:
+			tweet_id (int): the id of the tweet to be retrieved using API
+		Returns:
+			either the tweet content (full text) or an empty string
+		"""
+		try:
+			return self.api.get_status(tweet_id, tweet_mode='extended').full_text
+		except:
+			return "" # Not Found
+
+	def get_tweets(self, ids):
+		"""
+		This method takes a list of tweet ids and returns a list of 
+		text within these tweets using parallel programming
+		Args:
+			ids (list): list of tweet ids taken from raw data
+		Returns:
+			list of tweet texts.
+		"""
+		result = Parallel(n_jobs=4, backend="threading")(
+						delayed(self.__get_tweet_by_id)(id_) \
+							for id_ in tqdm(ids, desc="Retrieving Tweets"))
+		return result
+
+
+if __name__ == "__main__":
+	t = TwitterCaller()
+	tweet_id = 421000000000000000
+	print(t.get_tweets([tweet_id]))
